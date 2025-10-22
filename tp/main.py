4915ab3bbee712827b54b2ae3ebfb6f3c4cf5e34
Martin Klöckner <mjkloeckner@gmail.com>
2025-10-22 17:00:47 -0300

    Fix `time_graph_multiple_data` hardcoded save name

--- tp/main.py
+++ tp/main.py
@@ -32,38 +32,47 @@ def time_domain_cancion1():
 
     ### porciones cuasi-periodicas 'cancion1'
     time_plot(file1_fs, file1_data, "cancion1_0_248s_a_0_256s",
-               t_start=0.248, t_width=0.008, a=0.24978, da=0.003)
+               t=0.248, dt=0.008, a=0.24978, da=0.003)
 
     time_plot(file1_fs, file1_data, "cancion1_0_520s_a_0_528s",
-               t_start=0.520, t_width=0.008, a=0.5208, da=0.003)
+               t=0.520, dt=0.008, a=0.5208, da=0.003)
 
     ### salida de filtro 'cancion1'
-    save_convolved_to_wav(file1_filter1_output, file1_fs, "file1_filter1_output.wav")
-    save_convolved_to_wav(file1_filter2_output, file1_fs, "file1_filter2_output.wav")
+    save_convolved_to_wav(file1_filter1_output, file1_fs,
+                          "file1_filter1_output.wav")
+
+    save_convolved_to_wav(file1_filter2_output, file1_fs,
+                          "file1_filter2_output.wav")
 
     ### grafico comparando la muestra 1 original y filtrada 1
     data_arr = [normalize(file1_data), normalize(file1_filter1_output)]
     leg_arr = ['Señal de audio', 'Señal de audio filtrada']
 
-    time_plot_multiple(file1_fs, data_arr, leg_arr, "cancion1_filter1_output_compare.png")
-    time_plot_multiple(file1_fs, data_arr, leg_arr, "cancion1_filter1_output_compare_0_248_a_0_256", t=0.248, dt=0.008)
+    time_plot_multiple(file1_fs, data_arr, leg_arr,
+                       "cancion1_filter1_output_compare")
+
+    time_plot_multiple(file1_fs, data_arr, leg_arr,
+                       "cancion1_filter1_output_compare_0_248_a_0_256",
+                       t=0.248, dt=0.008)
 
     ### grafico comparando la muestra 1 original y filtrada 2
     data_arr = [normalize(file1_data), normalize(file1_filter2_output)]
     leg_arr = ['Señal de audio', 'Señal de audio filtrada']
 
-    time_plot_multiple(file1_fs, data_arr, leg_arr, "cancion1_filter2_output_compare")
     time_plot_multiple(file1_fs, data_arr, leg_arr,
-                       "cancion1_filter2_output_compare_0_248_a_0_256", t=0.248, dt=0.008)
+                       "cancion1_filter2_output_compare")
+    time_plot_multiple(file1_fs, data_arr, leg_arr,
+                       "cancion1_filter2_output_compare_0_248_a_0_256",
+                       t=0.248, dt=0.008)
 
 ## 'cancion2'
 def time_domain_cancion2():
     ### grafico completo
-    time_plot(file2_fs, file2_data, "cancion2", t_start=6)
+    time_plot(file2_fs, file2_data, "cancion2", t=6)
 
     ### porciones cuasi-periodicas 'cancion2'
-    time_plot(file2_fs, file2_data, "cancion2_14_72s_a_14_73s", t_start=14.720, t_width=0.01)
-    time_plot(file2_fs, file2_data, "cancion2_26_57s_a_26_58s", t_start=26.570, t_width=0.01)
+    time_plot(file2_fs, file2_data, "cancion2_14_72s_a_14_73s", t=14.720, dt=0.01)
+    time_plot(file2_fs, file2_data, "cancion2_26_57s_a_26_58s", t=26.570, dt=0.01)
 
     save_convolved_to_wav(file2_filter1_output, file2_fs, "file2_filter1_output.wav")
     save_convolved_to_wav(file2_filter2_output, file2_fs, "file2_filter2_output.wav")
@@ -72,15 +81,21 @@ def time_domain_cancion2():
     data_arr = [normalize(file2_data), normalize(file2_filter1_output)]
     leg_arr = ['Señal original', 'Señal filtrada']
 
-    time_plot_multiple(file2_fs, data_arr, leg_arr, "cancion2_6s_filter1_output_compare", t=6)
-    time_plot_multiple(file2_fs, data_arr, leg_arr, "cancion2_6s_filter1_output_compare_26_57_a_26_58", t=26.57, dt=0.01)
+    time_plot_multiple(file2_fs, data_arr, leg_arr,
+                       "cancion2_6s_filter1_output_compare", t=6)
+    time_plot_multiple(file2_fs, data_arr, leg_arr,
+                       "cancion2_6s_filter1_output_compare_26_57_a_26_58",
+                       t=26.57, dt=0.01)
 
     ### grafico comparando la muestra 2 original y filtrada 2
     data_arr = [normalize(file2_data), normalize(file2_filter2_output)]
     leg_arr = ['Señal original', 'Señal filtrada']
 
-    time_plot_multiple(file2_fs, data_arr, leg_arr, "cancion2_6s_filter2_output_compare", t=6)
-    time_plot_multiple(file2_fs, data_arr, leg_arr, "cancion2_6s_filter2_output_compare_26_57_a_26_58", t=26.57, dt=0.01)
+    time_plot_multiple(file2_fs, data_arr, leg_arr,
+                       "cancion2_6s_filter2_output_compare", t=6)
+    time_plot_multiple(file2_fs, data_arr, leg_arr,
+                       "cancion2_6s_filter2_output_compare_26_57_a_26_58",
+                       t=26.57, dt=0.01)
 
 def time_domain_music_instruments():
     ## Sonido de instrumentos musicales
@@ -89,9 +104,9 @@ def time_domain_music_instruments():
     a4_violin_fs, a4_violin_data = wavfile.read(a4_violin_file_path)
 
     ### grafico de los instrumentos musicales
-    time_plot(a4_flauta_fs, a4_flauta_data, "a4_flauta", t_start=0.25, t_width=0.010)
-    time_plot(a4_clarinete_fs, a4_clarinete_data, "a4_clarinete", t_start=0.25, t_width=0.010)
-    time_plot(a4_violin_fs, a4_violin_data, "a4_violin", t_start=0.25, t_width=0.010)
+    time_plot(a4_flauta_fs, a4_flauta_data, "a4_flauta", t=0.25, dt=0.010)
+    time_plot(a4_clarinete_fs, a4_clarinete_data, "a4_clarinete", t=0.25, dt=0.010)
+    time_plot(a4_violin_fs, a4_violin_data, "a4_violin", t=0.25, dt=0.010)
 
 def time_domain():
     time_domain_cancion1()
@@ -120,10 +135,15 @@ def freq_domain():
     freq_plot(48000, filter1_h, "filter1_h_fft", f_max=2000)
     freq_plot(48000, filter2_h, "filter2_h_fft", f_max=8000)
 
-    for i in [512, 1024, 2048, 4096]:
+    for i in [512, 1024, 2048]:
         for window in ['hann', 'hamming', 'blackman']:
-            spectogram_plot(file1_fs, file1_data, f"cancion1_espectograma_{window}_{i:04d}", N=i, win=window)
-            spectogram_plot(file2_fs, file2_data, f"cancion2_espectograma_{window}_{i:04d}", t=6, N=i, win=window)
+            spectogram_plot(file1_fs, file1_data,
+                            f"cancion1_espectograma_{window}_{i:04d}", N=i,
+                            win=window, ylim=[0, 17500])
+
+            spectogram_plot(file2_fs, file2_data,
+                            f"cancion2_espectograma_{window}_{i:04d}", t=6, N=i,
+                            win=window, ylim=[0, 8000])
 
 time_domain()
 freq_domain()
